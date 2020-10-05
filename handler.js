e23e230cdcb873210e527e2f81a2b8f41be3db85
Graham <grahamfalconer@icloud.com>
2020-10-05 15:26:21 +0100

    added get one function

--- handler.js
+++ handler.js
@@ -28,3 +28,24 @@ module.exports.create = (event, context, callback) => {
 			);
 	});
 };
+
+module.exports.getOne = (event, context, callback) => {
+	context.callbackWaitsForEmptyEventLoop = false;
+
+	connectToDatabase().then(() => {
+		Note.findById(event.pathParameters.id)
+			.then(note =>
+				callback(null, {
+					statusCode: 200,
+					body: JSON.stringify(note)
+				})
+			)
+			.catch(err =>
+				callback(null, {
+					statusCode: err.statusCode || 500,
+					headers: { 'Content-Type': 'text/plain' },
+					body: 'Could not fetch the note.'
+				})
+			);
+	});
+};
