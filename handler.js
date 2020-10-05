0e8d02d0a35a1f654c9a3bb2d02e65ca87a444c9
Graham <grahamfalconer@icloud.com>
2020-10-05 15:27:57 +0100

    added delete function

--- handler.js
+++ handler.js
@@ -93,3 +93,27 @@ module.exports.update = (event, context, callback) => {
 			);
 	});
 };
+
+module.exports.delete = (event, context, callback) => {
+	context.callbackWaitsForEmptyEventLoop = false;
+
+	connectToDatabase().then(() => {
+		Note.findByIdAndRemove(event.pathParameters.id)
+			.then(note =>
+				callback(null, {
+					statusCode: 200,
+					body: JSON.stringify({
+						message: 'Removed note with id: ' + note._id,
+						note: note
+					})
+				})
+			)
+			.catch(err =>
+				callback(null, {
+					statusCode: err.statusCode || 500,
+					headers: { 'Content-Type': 'text/plain' },
+					body: 'Could not fetch the notes.'
+				})
+			);
+	});
+};
